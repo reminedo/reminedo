643ca4e25fce6226639880ea8e0950e101ee89cb
geniusjun <geniusjun4663@gmail.com>
2026-06-17 17:07:55 +0900

    Initial Commit

---
+++ reminedo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  reminedo
+//
+//  Created by 노창준 on 6/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ reminedo/reminedoApp.swift
@@ -0,0 +1,17 @@
+//
+//  reminedoApp.swift
+//  reminedo
+//
+//  Created by 노창준 on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct reminedoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
